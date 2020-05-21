991e7d2a0e4fba91723e88fb72cb503929b32ba3
Eugene Vlasov <negativ529021@gmail.com>
2020-05-21 23:16:01 +0300

    Initial commit

---
+++ src/main/java/ua/vlasov_eugene/plexsupply1/Plexsupply1Application.java
@@ -0,0 +1,13 @@
+package ua.vlasov_eugene.plexsupply1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plexsupply1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plexsupply1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/vlasov_eugene/plexsupply1/Plexsupply1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vlasov_eugene.plexsupply1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plexsupply1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
