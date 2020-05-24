46a2e30f602d0f8a227420c323c03917d7b75ca9
Eugene Vlasov <negativ529021@gmail.com>
2020-05-24 13:35:59 +0300

    Test-coverage of FileSender now is 100%

--- src/test/java/ua/vlasov_eugene/plexsupply1/util/FileSenderTest.java
+++ src/test/java/ua/vlasov_eugene/plexsupply1/util/FileSenderTest.java
@@ -2,25 +2,27 @@ package ua.vlasov_eugene.plexsupply1.util;
 
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.apache.commons.net.ftp.FTPClient;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Value;
+import ua.vlasov_eugene.plexsupply1.exceptions.FTPWriteException;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FileSenderTest {
+	private static Date date;
+	private static String uuid;
 	private static final FastDateFormat MONTH_FORMAT = FastDateFormat.getInstance("yyyy-MM");
 	private static final String SUCCESS_FILENAME = "successFile.txt";
 	private static final File SUCCESS_FILE = new File("src/test/resources/fileDirectory/successFile.txt");
@@ -38,19 +40,23 @@ public class FileSenderTest {
 	@InjectMocks
 	private FileSender testedClass;
 
+	@BeforeClass
+	public static void init(){
+		date = new Date();
+		uuid = java.util.UUID.randomUUID().toString();
+	}
+
 	@Test
 	public void sendFileExpectedSuccess() throws IOException {
-		Date currentDate = new Date();
-		String UUID = java.util.UUID.randomUUID().toString();
 		String expectedMessage = "success read\n" +
 				" File successFile.txt was upload successfully! " +
 				"The link on file is: file/" +
-				UUID + "/" +
-				MONTH_FORMAT.format(currentDate) + "/successFile.txt";
+				uuid + "/" +
+				MONTH_FORMAT.format(date) + "/successFile.txt";
 
 
-		when(creator.getCurrentDate()).thenReturn(currentDate);
-		when(creator.getUUID()).thenReturn(UUID);
+		when(creator.getCurrentDate()).thenReturn(date);
+		when(creator.getUUID()).thenReturn(uuid);
 
 		assertEquals(expectedMessage,testedClass.sendFile(SUCCESS_FILENAME,SUCCESS_FILE,SUCCESS_RESULT));
 
@@ -60,5 +66,21 @@ public class FileSenderTest {
 		verify(client).storeFile(anyString(),any());
 		verify(client).logout();
 		verify(client).disconnect();
+	}
+
+	@Test(expected = FTPWriteException.class)
+	public void sendFileExpectdException() throws IOException {
+		when(creator.getCurrentDate()).thenReturn(date);
+		when(creator.getUUID()).thenReturn(uuid);
+		when(client.storeFile(anyString(),any())).thenThrow(new IOException("some message"));
+
+		testedClass.sendFile(SUCCESS_FILENAME,SUCCESS_FILE,SUCCESS_RESULT);
+
+		verify(client).connect(hostAddress);
+		verify(client).enterLocalPassiveMode();
+		verify(client).login(login,password);
+		verify(client).storeFile(anyString(),any());
+		verify(client,times(0)).logout();
+		verify(client,times(0)).disconnect();
 	}
 }
